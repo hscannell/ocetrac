c7b9b44fc80cc75f5b2a10e952e665e571d9f06a
“hscannell” <scannell@ldeo.columbia.edu>
2021-01-25 08:43:45 -0700

    overdue commit

--- ocetrac/_nbdev.py
+++ ocetrac/_nbdev.py
@@ -2,8 +2,7 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"label_blobs": "00_core.ipynb",
-         "track_blobs": "00_core.ipynb"}
+index = {"track": "00_core.ipynb"}
 
 modules = ["core.py"]
 
